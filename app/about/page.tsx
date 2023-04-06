4f73b19cc83f91a95eca7bc7cc5dbc83831ec930
Robert Gleason <l2ob122@gmail.com>
2023-04-05 20:04:14 -0700

    created 2 fetch requests using Client Side Rendering and Server Side Rendering

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,29 @@
+// SERVER SIDE RENDERING
+
 import Link from 'next/link';
+
+const fetchTodos = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  return data;
+};
 
 export const metadata = {
   title: 'About',
   description: 'About page',
 };
 
-export default function About() {
+export default async function About() {
+  const data = await fetchTodos();
   return (
     <main>
       <Link href={'/'}>Navigate To Home Page</Link>
       <h1>About Page</h1>
+      {data.map((todo) => (
+        <div>
+          <h3>{todo.title}</h3>
+        </div>
+      ))}
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+// Client-side rendering
+
+'use client';
+
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await res.json();
+      setData(data);
+    };
+    fetchTodos();
+  });
   return (
     <main>
       <Link href={'/about'}>Navigate To About</Link>
       <h1>Hello next 13</h1>
+      {data.map((todo) => (
+        <div>
+          <h3>{todo.title}</h3>
+        </div>
+      ))}
     </main>
   );
 }
